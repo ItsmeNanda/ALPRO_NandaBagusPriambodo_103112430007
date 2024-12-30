dbc90cbd515250ee7036ef38666b0b71f5b92a41
Nanda Bagus Priambodo <110284713+ItsmeNanda@users.noreply.github.com>
2024-12-31 01:47:54 +0700

    Add files via upload

---
+++ AlltaskBeforeFinal/Week 13 & 14/soal10PolaX.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+
+	fmt.Print("Masukkan bilangan bulat ganjil: ")
+	fmt.Scan(&n)
+
+	if n%2 == 0 {
+		fmt.Println("Input harus bilangan bulat ganjil.")
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if j == i || j == n-i-1 {
+				fmt.Print(i + 1)
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
